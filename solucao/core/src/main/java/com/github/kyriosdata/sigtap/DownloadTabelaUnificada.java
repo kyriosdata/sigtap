3f61798656ae95cd6a48fb76441b454ac3f164d2
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2020-03-18 20:21:37 -0300

    testes

--- solucao/core/src/main/java/com/github/kyriosdata/sigtap/DownloadTabelaUnificada.java
+++ solucao/core/src/main/java/com/github/kyriosdata/sigtap/DownloadTabelaUnificada.java
@@ -3,23 +3,86 @@ package com.github.kyriosdata.sigtap;
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class DownloadTabelaUnificada {
 
+    private static final Logger logger =
+            LoggerFactory.getLogger(DownloadTabelaUnificada.class);
     public static final String FTP_SERVER = "ftp2.datasus.gov.br";
-    public static final String DIRECTORY = "/pub/sistemas/tup/downloads";
+    public static final String DIRECTORY = "/pub/sistemas/tup/downloads/";
     public static final String PREFIXO = "TabelaUnificada_%s_v";
 
-    public static void main(String[] args) {
-        List<String> arquivos = getNomesArquivosPara("202003");
-        arquivos.stream().forEach(System.out::println);
+    /**
+     * Executa download do arquivo contendo a tabela unificada de procedimentos
+     * para a competência fonecida no formato (yyyymm). O arquivo
+     * correspondente é depositado no diretório indicado.
+     *
+     * @param competencia Competência no formato yyyyMM. Ou seja, ano
+     *                    fornecido (4 dígitos) e mês usando dois dígitos.
+     *                    Por exemplo¸ para janeiro de 2001 a competência
+     *                    deve ser "200101".
+     * @param dir O diretório onde o arquivo contendo a tabela unificada de
+     *            procedimentos para a competência indicada será depositado.
+     *
+     * @return Instância para o arquivo depositado no diretório indicado se o
+     * download foi executado de forma satisfatória. Ou {@code null}, caso
+     * nenhum arquivo seja encontrado para a competência fornecida.
+     *
+     * @throws RuntimeException Se ocorreu situação excepcional que impede o
+     * download da tabela unificada.
+     */
+    public static File baixar(String competencia, String dir) {
+        List<String> arquivos = getNomesArquivosPara(competencia);
+        if (arquivos.isEmpty()) {
+            logger.info("não foi localizado arquivo para competência {}",
+                    competencia);
+            return null;
+        }
 
+        final String arquivo = arquivos.get(0);
+
+        if (arquivos.size() > 1) {
+            logger.warn("mais de um arquivo para competência {}, usando " +
+                    "arquivo {}", competencia, arquivo);
+        }
+
+        File file = downloadTabela(arquivo, dir);
+        logger.info("arquivo {} baixado para competencia {}", arquivo,
+                competencia);
+
+        return file;
+    }
+
+    public static File downloadTabela(String arquivo,
+                                      String dir) {
+        FTPClient ftpClient = new FTPClient();
+        try {
+            ftpClient.connect(FTP_SERVER);
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+            ftpClient.login("anonymous", "");
+
+            File file = new File(dir + File.separator + arquivo);
+            FileOutputStream fos = new FileOutputStream(file);
+            OutputStream os = new BufferedOutputStream(fos);
+            ftpClient.retrieveFile(DIRECTORY + arquivo, os);
+            ftpClient.logout();
+            ftpClient.disconnect();
+            return file;
+        } catch (IOException exp) {
+            logger.error(exp.getMessage());
+            throw new RuntimeException("erro ao realizar download");
+        }
     }
 
     public static List<String> getNomesArquivosPara(String competencia) {
@@ -42,7 +105,8 @@ public class DownloadTabelaUnificada {
 
             return arquivos;
         } catch (IOException exp) {
-            throw new RuntimeException(exp);
+            logger.error(exp.getMessage());
+            throw new RuntimeException("erro ao listar arquivos de ftp server");
         }
     }
 }
--- solucao/core/src/test/java/com/github/kyriosdata/sigtap/domain/DownloadTabelaUnificadaTest.java
+++ solucao/core/src/test/java/com/github/kyriosdata/sigtap/domain/DownloadTabelaUnificadaTest.java
@@ -6,7 +6,28 @@
 
 package com.github.kyriosdata.sigtap.domain;
 
+import com.github.kyriosdata.sigtap.DownloadTabelaUnificada;
+import org.assertj.core.util.Files;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class DownloadTabelaUnificadaTest {
+
+    @Test
+    public void competenciaInvalida() {
+        assertNull(DownloadTabelaUnificada.baixar("203", "dir"));
+    }
+
+    @Test
+    public void competenciaExistente() {
+        String dir = Files.temporaryFolderPath();
+        File arquivo = DownloadTabelaUnificada.baixar("202003", dir);
+        assertTrue(arquivo.getName().contains("_202003_"));
+    }
 }
 
 
