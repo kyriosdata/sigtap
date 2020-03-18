5f2ba80e3b6f2454eecb88557d71374aa3ca748e
Fabio Nogueira de Lucena <fabio@inf.ufg.br>
2020-03-18 18:37:05 -0300

    obtendo nomes de arquivos para competencia

--- solucao/core/src/main/java/com/github/kyriosdata/sigtap/DownloadTabelaUnificada.java
+++ solucao/core/src/main/java/com/github/kyriosdata/sigtap/DownloadTabelaUnificada.java
@@ -6,30 +6,43 @@ import org.apache.commons.net.ftp.FTPFile;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class DownloadTabelaUnificada {
 
     public static final String FTP_SERVER = "ftp2.datasus.gov.br";
     public static final String DIRECTORY = "/pub/sistemas/tup/downloads";
+    public static final String PREFIXO = "TabelaUnificada_%s_v";
 
-    public static void main(String[] args) throws Exception {
-        getTabelaPara();
-        return;
+    public static void main(String[] args) {
+        List<String> arquivos = getNomesArquivosPara("202003");
+        arquivos.stream().forEach(System.out::println);
+
     }
 
-    public static void getTabelaPara() throws IOException {
+    public static List<String> getNomesArquivosPara(String competencia) {
+        final String prefixo = String.format(PREFIXO, competencia);
         FTPClient ftpClient = new FTPClient();
-        ftpClient.connect(FTP_SERVER);
-        ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-        ftpClient.login("anonymous", "");
+        try {
+            ftpClient.connect(FTP_SERVER);
+            ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+            ftpClient.login("anonymous", "");
 
-        FTPFile[] files = ftpClient.listFiles(DIRECTORY);
+            FTPFile[] files = ftpClient.listFiles(DIRECTORY);
 
-        Arrays.stream(files)
-                .filter(f -> f.getName().startsWith("TabelaUnificada"))
-                .forEach(System.out::println);
+            List<String> arquivos = Arrays.stream(files)
+                    .filter(f -> f.getName().startsWith(prefixo))
+                    .map(s -> s.getName())
+                    .collect(Collectors.toList());
 
-        ftpClient.logout();
-        ftpClient.disconnect();
+            ftpClient.logout();
+            ftpClient.disconnect();
+
+            return arquivos;
+        } catch (IOException exp) {
+            throw new RuntimeException(exp);
+        }
     }
 }
