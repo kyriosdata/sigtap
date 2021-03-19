41049dd2458e8f7c009a9d1bb41abbee1d92088c
kyriosdata <fabio@inf.ufg.br>
2021-03-19 11:09:42 -0300

    await

--- solucao/sigtap-js/sigtapzip.js
+++ solucao/sigtap-js/sigtapzip.js
@@ -3,26 +3,46 @@ const fs = require("fs");
 
 const zipfile = "../../datasus/TabelaUnificada_202103_v2103031426.zip";
 
-async function getConteudo(zfn) {
-  const zip = await jszip.loadAsync(zfn);
-  zip.forEach((path, entry) => {
-    console.log(path);
-  });
-}
+class SigtapZip {
+  constructor(zipFilename) {
+    this.filename = zipFilename;
+    this.blob = this.readZip(zipfile);
+  }
 
-function readZip(zfn) {
-  try {
-    if (fs.existsSync(zfn)) {
-      console.log("Arquivo encontrado...");
-      return fs.readFileSync(zfn);
+  async conteudo() {
+    const zip = await jszip.loadAsync(this.blob);
+    zip.forEach((path) => {
+      console.log(path);
+    });
+  }
+
+  async carregue(entrada) {
+    const zip = await jszip.loadAsync(this.blob);
+    const conteudo = await zip.file(entrada).async("string");
+    console.log(conteudo);
+  }
+
+  readZip(zfn) {
+    try {
+      if (fs.existsSync(zfn)) {
+        console.log("Arquivo encontrado...");
+        return fs.readFileSync(zfn);
+      }
+    } catch (err) {
+      console.error(err);
+      return "erro ao executar SigtapZip.readZip";
     }
-  } catch (err) {
-    console.error(err);
-    return undefined;
   }
 }
 
-const blob = readZip(zipfile);
-getConteudo(blob)
+const sigtap = new SigtapZip(zipfile);
+
+sigtap
+  .conteudo()
   .then(() => console.log("concluido"))
   .catch(console.log);
+
+sigtap
+  .carregue("LEIA_ME.TXT")
+  .then(console.log)
+  .catch(() => console.log("erro"));
